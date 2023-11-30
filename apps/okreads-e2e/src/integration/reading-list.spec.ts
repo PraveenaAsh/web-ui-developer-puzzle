8e69422a9caa9846deac51572f511cacd7c54e9e
praveena <praveena.avula18@gmail.com>
2023-11-30 17:53:05 +0530

    Updated review comments

--- apps/okreads-e2e/src/integration/reading-list.spec.ts
+++ apps/okreads-e2e/src/integration/reading-list.spec.ts
@@ -1,76 +1,56 @@
-import { $, $$, browser, By, ExpectedConditions } from 'protractor';
-
 describe('When: I use the reading list feature', () => {
-  beforeEach(async () => {
-    await browser.get('/');
-    await browser.wait(
-      ExpectedConditions.textToBePresentInElement($('tmo-root'), 'okreads')
+  let readingItemsCount = 0;
+  beforeEach(() => {
+    cy.startAt('/');
+    readingItemsCount = cy.$$('[data-testing="reading-list-item"]').length;
+  });
+
+  it('Then: I should see my reading list', () => {
+    cy.get('[data-testing="toggle-reading-list"]').click();
+
+    cy.get('[data-testing="reading-list-container"]').should(
+      'contain.text',
+      'My Reading List'
     );
   });
 
-  it('Then: I should see my reading list', async () => {
-    const readingListToggle = await $('[data-testing="toggle-reading-list"]');
-    await readingListToggle.click();
+  it('Then: I should be able to add book and undo added book', () => {
 
-    await browser.wait(
-      ExpectedConditions.textToBePresentInElement(
-        $('[data-testing="reading-list-container"]'),
-        'My Reading List'
-      )
-    );
+    cy.get('input[type="search"]').type('angular');
+
+    cy.get('form').submit();
+
+    cy.get('[data-testing="add-to-reading-list-button"]:enabled').first().click();
+
+    cy.get('[data-testing="reading-list-item"]').should('have.length', readingItemsCount + 1);
+
+    cy.get('.mat-simple-snackbar-action button').click();
+
+    cy.get('[data-testing="reading-list-item"]').should('have.length', readingItemsCount);
+
   });
 
-  it('Then: I should be able to add an item to the list and then undo', async () => {
-    /* search form */
-    await $('input[type="search"]').sendKeys('Blockchain');
-    await $('form').submit();
+  it('Then: I should be able to remove book and undo removed book', () => {
+    cy.get('input[type="search"]').type('angular');
 
-    /**
-     * adding book to reading list
-     * required to cover the edge case scenario when reading list is empty
-     */
-    await $$('[data-testing="book-add-button"][ng-reflect-disabled="false"]').first().click();
+    cy.get('form').submit();
 
-    /* initial count of reading list item */
-    const readingListItem = $('tmo-total-count [ng-reflect-content]');
-    const initialItemCount = await readingListItem.getAttribute('ng-reflect-content');
+    cy.get('[data-testing="add-to-reading-list-button"]:enabled').first().click();
 
-    /* adding another book to reading list */
-    await $$('[data-testing="book-add-button"][ng-reflect-disabled="false"]').first().click();
+    cy.get('[data-testing="reading-list-item"]').should('have.length', readingItemsCount + 1);
 
-    /* undo action */
-    await browser.driver.findElement(By.className('mat-simple-snackbar-action')).click();
+    cy.get('[data-testing="toggle-reading-list"]').click();
 
-    /* final count of reading list */
-    const finalItemCount = await readingListItem.getAttribute('ng-reflect-content');
+    cy.get('[data-testing="remove-from-reading-list-button"]:enabled').first().click();
 
-    expect(finalItemCount).toEqual(initialItemCount);
+    cy.get('[data-testing="close-reading-list"]').click();
+
+    cy.get('[data-testing="reading-list-item"]').should('have.length', readingItemsCount);
+
+    //undo remove from reading list
+    cy.get('.mat-simple-snackbar-action button').click();
+
+    cy.get('[data-testing="reading-list-item"]').should('have.length', readingItemsCount + 1);
   });
 
-  it('Then: I should be able to remove an item from the list and then undo', async () => {
-    /* search form */
-    await $('input[type="search"]').sendKeys('Big Data');
-    await $('form').submit();
-
-    /* add book to reading list */
-    await $$('[data-testing="book-add-button"][ng-reflect-disabled="false"]').first().click();
-
-    /* open reading list */
-    await $('[data-testing="toggle-reading-list"]').click();
-
-    /* initial count of reading list item */
-    const readingListItem = $('tmo-total-count [ng-reflect-content]');
-    const initialItemCount = await readingListItem.getAttribute('ng-reflect-content');
-
-    /* remove book from reading list */
-    await $$('[data-testing="book-remove-button"]').last().click();
-
-    /* undo action */
-    await browser.driver.findElement(By.className('mat-simple-snackbar-action')).click();
-
-    /* final count of reading list item */
-    const finalItemCount = await readingListItem.getAttribute('ng-reflect-content');
-
-    expect(finalItemCount).toEqual(initialItemCount);
-  });
 });
