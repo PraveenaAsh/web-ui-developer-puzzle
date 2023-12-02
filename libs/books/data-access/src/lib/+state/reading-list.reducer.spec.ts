2d2994b064fa5a40669be5f4bed99628dfb5a98f
praveena <praveena.avula18@gmail.com>
2023-12-03 00:01:03 +0530

    implemented changes to mark a book as finished

--- libs/books/data-access/src/lib/+state/reading-list.reducer.spec.ts
+++ libs/books/data-access/src/lib/+state/reading-list.reducer.spec.ts
@@ -51,6 +51,33 @@ describe('Books Reducer', () => {
 
       expect(result.ids).toEqual(['A', 'B', 'C']);
     });
+
+    it('markBookAsFinished should set finished and finishedDate in state', () => {
+      const finishedDate = new Date().toISOString();
+      const item = createReadingListItem('B');
+      const action = ReadingListActions.markBookAsFinished({
+        bookId: item.bookId,
+        finishedDate
+      });
+  
+      const result: State = reducer(state, action);
+  
+      expect(result.entities[item.bookId].finishedDate).toEqual(finishedDate);
+      expect(result.entities[item.bookId].finished).toBe(true);
+    });
+  
+    it('markBookAsFinishedFailed should set finished as false and finishedDate as empty in state', () => {
+      const item = createReadingListItem('B');
+      const action = ReadingListActions.markBookAsFinishedFailed({
+        bookId: item.bookId
+      });
+  
+      const result: State = reducer(state, action);
+  
+      expect(result.entities[item.bookId].finishedDate).toEqual('');
+      expect(result.entities[item.bookId].finished).toBe(false);
+    });
+
   });
 
   describe('unknown action', () => {
